13f7eda4595a85cdf4a96753040e65073a55949d
Mukund Tonge <mukundtonge147@gmail.com>
2019-11-13 15:43:03 +0530

    Added something

--- product.js
+++ product.js
@@ -16,15 +16,15 @@ app.get('/', (request, response) => {
     response.send('welcome to my application')
 })
 
-// app.get('/product', (request, response) => {
-//     const connection = connect()
-//     const statement = `select * from Product`
-//     connection.query(statement, (error, products) => {
-//         connection.end()
-//         response.send(products)
-//     })
-// })
+app.get('/product', (request, response) => {
+    const connection = connect()
+    const statement = `select * from Product`
+    connection.query(statement, (error, products) => {
+        connection.end()
+        response.send(products)
+    })
+})
 
 app.listen(4000, () => {
-    console.log(`Server started on port 4000`);
+    console.log(`Server started on port 4000 Kapil Suryawanshi`);
 });
